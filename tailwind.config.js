c4036d735cc6d9a398574ab3aa9e24e4328f6071
Anurag <danurag1906@gmail.com>
2022-11-30 22:12:44 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        BigShoulder:["Big Shoulders Display","cursive"],
+        lexend:["Lexend Deca","sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
